d31623331a57c1e0067de5d0e559347f934a1b58
Javier De la Hoz <javierdlahoz@gmail.com>
2015-05-21 10:36:05 -0500

    new things

--- bin/shellHelper.php
+++ bin/shellHelper.php
@@ -1,6 +1,11 @@
 <?php
 $file = $_POST["file"];
-$info = exec("sudo sh ".__DIR__."/../shell/".$file);
+if($_POST["file"] == "easy-push.sh"){
+    $info = exec("sh ".__DIR__."/../shell/".$file);
+}
+else{
+    $info = exec("sudo sh ".__DIR__."/../shell/".$file);
+}
 header('Content-Type: application/json');
 echo json_encode(array("message" => $info));
 exit;
